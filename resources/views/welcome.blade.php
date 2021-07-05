402be70aae6d4d60e75380c1152f0f601183f2e2
kei <djordjedevelopment@gmail.com>
2021-07-05 13:36:09 +0900

    welcome loading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -277,6 +277,25 @@
       $("div#featured_img").css('right', parseInt(pos) + 1)
     }
   }
+
+  var imgs = document.images,
+    len = imgs.length,
+    counter = 0;
+
+  [].forEach.call( imgs, function( img ) {
+      if(img.complete)
+        incrementCounter();
+      else
+        img.addEventListener( 'load', incrementCounter, false );
+  } );
+
+  function incrementCounter() {
+      counter++;
+      if ( counter === len ) {
+          console.log( 'All images loaded!' );
+      }
+  }
+  
 </script>
 
 </html>
